3a18e2d541c3b963e855cbce30fe1bbc7ce09d6f
adebayox <dadebayo200@gmail.com>
2022-03-18 22:34:31 +0100

    Complete project on to-do list

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Task  from './component/Task';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      {/*Today's task */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}>
+          <Task text={'Read CSC 411'} />
+          <Task text={'Go to chapel'}/>
+          <Task text={'Go to Library'}/>
+
+        </View>
+
+      </View>
+      
     </View>
   );
 }
@@ -13,8 +28,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+    
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+sectionTitle: {
+  fontSize: 24,
+  fontWeight: 'bold'
+},
+items: {
+  marginTop: 30,
+},
 });
